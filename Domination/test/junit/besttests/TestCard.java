4be2e711c4932bd01db50ec8e99fe98540d20bb7
Jon O'Brien <jvo7822@rit.edu>
2016-04-10 19:31:26 -0400

    added more card tests

--- Domination/test/junit/besttests/TestCard.java
+++ Domination/test/junit/besttests/TestCard.java
@@ -34,7 +34,39 @@ public class TestCard {
     }
 
 
+    // test for setName exception
+    @Test
+    public void testSetNameThrow() {
+        Continent testContinent = new Continent("continentString1", "continentString2",1234,4321);
+        Country testCountry = new Country(1,"1","2",testContinent, 1,1);
+        try {
+            Card testCard = new Card("errorType", testCountry);
+            org.junit.Assert.fail("IllegalArgumentException should be thrown");
+        }
+        catch (IllegalArgumentException e) {
+            // success if thrown
+        }
+    }
 
+
+    @Test
+    public void testGetCountry() {
+        Continent testContinent = new Continent("continentString1", "continentString2",1234,4321);
+        Country testCountry = new Country(1,"1","2",testContinent, 1,1);
+        Card testCard = new Card("Cavalry", testCountry);
+        assertEquals(testCountry,testCard.getCountry());
+    }
+
+
+    @Test
+    public void testSetContry() {
+        Continent testContinent = new Continent("continentString1", "continentString2",1234,4321);
+        Country testCountry = new Country(1,"1","2",testContinent, 1,1);
+        Country testCountryTwo = new Country(2,"3","4",testContinent, 2,2);
+        Card testCard = new Card("Cavalry", testCountry);
+        testCard.setCountry(testCountryTwo);
+        assertEquals(testCountryTwo,testCard.getCountry());
+    }
 
 
 }
